bd3714352eba0deb9ec0ed243cba5745bbd4db16
Robert Dezmerean <robert.dezmerean@altom.com>
2019-11-08 07:21:31 +0000

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/model-editor';
+    ENV.rootURL = 'altwalker/model-editor/';
     ENV.locationType = 'hash';
   }
 
