01f6863265d4357f773790d3e565e42d1ae8b0e7
Robert Dezmerean <robert.dezmerean@altom.com>
2020-05-28 08:21:19 +0000

    Resolve "Add titles to all pages"

--- app/routes/json-editor.js
+++ app/routes/json-editor.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default class JsonEditorRoute extends Route {
+  @service headData
+
+  afterModel() {
+    set(this, 'headData.img', 'json-editor.png');
+    set(this, 'headData.url', this.routeName.replace(".", "/"));
+  }
 }
--- app/routes/visual-editor.js
+++ app/routes/visual-editor.js
@@ -1,12 +1,19 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default class VisualEditorRoute extends Route {
   @service modelStorage;
+  @service headData
 
   beforeModel() {
     if (this.modelStorage.isPlottingError) {
       this.transitionTo('json-editor');
     }
+  }
+
+  afterModel() {
+    set(this, 'headData.img', 'visual-editor.png');
+    set(this, 'headData.url', this.routeName.replace(".", "/"));
   }
 }
