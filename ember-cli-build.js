56b52bca42995d93bd1dcd6204843afb21e79ee5
Robert Dezmerean <robert.dezmerean@altom.com>
2020-08-06 18:25:03 +0300

    Update ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,17 +7,10 @@ const cssImport = require('postcss-import');
 module.exports = function(defaults) {
   let config = defaults.project.config(EmberApp.env());
 
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-  console.log("rootURL", config.rootURL)
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-
   let app = new EmberApp(defaults, {
     fingerprint: {
       exclude: [
-        'assets/screenshots/',
-        'apple-touch-icon',
-        'favicon',
-        'mstile'
+        'assets/screenshots/'
       ]
     },
     postcssOptions: {
@@ -33,7 +26,7 @@ module.exports = function(defaults) {
     'ember-cli-favicon': {
       iconPath: 'assets/icons/favicon.png',
       faviconsConfig: {
-        // path: config.rootURL
+        path: `/${config.rootURL}`
       }
     },
   });
